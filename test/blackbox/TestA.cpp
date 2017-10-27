00ec5dc1e286cf170c61b62517308c8cd67b6289
Vicente Monge <vicentemonge@eprosima.com>
2017-10-27 13:57:55 +0200

    Base test for integration

---
+++ test/blackbox/TestA.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "MessageOutput.h"
+
+int main(int argc, char *argv[])
+{
+    printf("\nAt the very beginning everything was black\n\n");
+
+
+    printf("exiting...\n");
+    return -1;
+}
---
+++ test/blackbox/TestB.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <cstdint>
+
+#include "MessageOutput.h"
+
+int main(int argc, char *argv[])
+{
+    printf("\nAt the very beginning everything was black\n\n");
+
+
+    printf("exiting...\n");
+    return 0;
+}
