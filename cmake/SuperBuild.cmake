c27f4d1cb2d10a656589786c2837d711298bc95c
julianbermudez <julianbermudez@eprosima.com>
2019-06-18 10:56:12 +0200

    Refs #5581. Attend pull request comments.

--- cmake/SuperBuild.cmake
+++ cmake/SuperBuild.cmake
@@ -73,9 +73,7 @@ if(NOT GTest_FOUND)
         GIT_REPOSITORY
             https://github.com/google/googletest.git
         GIT_TAG
-            release-1.8.1
-        GIT_SHALLOW
-            TRUE
+            2fe3bd994b3189899d93f1d5a881e725e046fdc2
         PREFIX
             ${PROJECT_BINARY_DIR}/googletest
         INSTALL_DIR
